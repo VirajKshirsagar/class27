dd2858f24a976aa79f2c87bee2e75952d26760f3
VirajKshirsagar <iamviraj2007@gmail.com>
2021-10-26 20:22:13 +0530

    we add sling shot

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var ss;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +37,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    ss=new SLING(bird.body,log4.body,300)
+
 }
 
 function draw(){
@@ -61,4 +64,6 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    ss.display();
 }
